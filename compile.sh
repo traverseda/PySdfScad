be1ae47c7b030aee0d6adcd6af4b1250832f87c2
traverseda <traverse.da@gmail.com>
2023-02-12 18:42:29 -0400

    Trying to get pyinstaller actions to work

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-poetry run pyinstaller pysdfscad_qtgui/main.py --onefile --name=pySdfScad --collect-data  pysdfscad --collect-data pysdfscad_qtgui --windowed
+QT_QPA_PLATFORM=linuxfb poetry run pyinstaller pysdfscad_qtgui/main.py --onefile --name=pySdfScad --collect-data  pysdfscad --collect-data pysdfscad_qtgui --windowed
