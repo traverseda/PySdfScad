935fa23777d49cfb5f359eb985f3835a38ac4a32
traverseda <traverse.da@gmail.com>
2023-02-12 18:45:30 -0400

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-QT_QPA_PLATFORM=linuxfb poetry run pyinstaller pysdfscad_qtgui/main.py --onefile --name=pySdfScad --collect-data  pysdfscad --collect-data pysdfscad_qtgui --windowed
+QT_QPA_PLATFORM=minimal poetry run pyinstaller pysdfscad_qtgui/main.py --onefile --name=pySdfScad --collect-data  pysdfscad --collect-data pysdfscad_qtgui --windowed
